0043a5960b0b8b991099ae9d1ed5c3db969ab293
Joseph Bauer <root@thesudorm.com>
2019-07-15 06:55:57 -0400

    Ensured that image is converted into RBG.

--- entries/thesudorm/main.py
+++ entries/thesudorm/main.py
@@ -2,9 +2,12 @@ import requests
 from PIL import Image
 from PIL import ImageColor
 
-im = Image.open("strawhat.png")
+im = Image.open("war.png")
+im = im.convert('RGB')
 width, height = im.size
 original_colors = im.getcolors()
+print("original")
+print(original_colors)
 
 response = requests.get('https://api.noopschallenge.com/hexbot?count=' + str(len(original_colors)))
 
@@ -12,17 +15,19 @@ hexbot_colors = []
 for color in response.json()['colors']:
     hexbot_colors.append(ImageColor.getrgb(color['value']))
 
-
 color_converter = {}
 
 x = 0
 for color in original_colors:
+    print(color)
     color_converter[color[1]] = hexbot_colors[x]
     x += 1
 
-print(color_converter)
 new_image = im
 
+print("Before making new image.")
 for x in range(width):
     for y in range(height):
-        color = new_image.getpixel((x, y))
+        new_image.putpixel((x,y), color_converter[im.getpixel((x,y))])
+
+new_image.save("test.png", "PNG")
