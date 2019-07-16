e7479f1a0054ce2364df82d0512b8595e45c3bf7
Joseph Bauer <root@thesudorm.com>
2019-07-15 22:26:21 -0400

    Started some refactoring for making more functionality.

--- entries/thesudorm/main.py
+++ entries/thesudorm/main.py
@@ -1,33 +1,39 @@
 import requests
+import click
+
 from PIL import Image
 from PIL import ImageColor
 
-im = Image.open("war.png")
-im = im.convert('RGB')
-width, height = im.size
-original_colors = im.getcolors()
-print("original")
-print(original_colors)
+def get_hexbot_colors(number):
+    response = requests.get('https://api.noopschallenge.com/hexbot?count=' + str(number))
 
-response = requests.get('https://api.noopschallenge.com/hexbot?count=' + str(len(original_colors)))
+    hexbot_colors = []
+    for color in response.json()['colors']:
+        hexbot_colors.append(ImageColor.getrgb(color['value']))
 
-hexbot_colors = []
-for color in response.json()['colors']:
-    hexbot_colors.append(ImageColor.getrgb(color['value']))
+    return hexbot_colors
+    
+@click.command()
+def random():
+    im = Image.open("war.png")
+    im = im.convert('RGB')
+    width, height = im.size
+    original_colors = im.getcolors()
 
-color_converter = {}
+    hexbot_colors = get_hexbot_colors(len(original_colors))
+    color_converter = {}
 
-x = 0
-for color in original_colors:
-    print(color)
-    color_converter[color[1]] = hexbot_colors[x]
-    x += 1
+    x = 0
+    for color in original_colors:
+        color_converter[color[1]] = hexbot_colors[x]
+        x += 1
 
-new_image = im
+    new_image = im
 
-print("Before making new image.")
-for x in range(width):
-    for y in range(height):
-        new_image.putpixel((x,y), color_converter[im.getpixel((x,y))])
+    for x in range(width):
+        for y in range(height):
+            new_image.putpixel((x,y), color_converter[im.getpixel((x,y))])
+    new_image.save("test.png", "PNG")
 
-new_image.save("test.png", "PNG")
+if __name__ == '__main__':
+    random()
